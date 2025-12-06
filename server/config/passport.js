81f5fa5efff94fced0f4053531d07ad4a3271963
Hilari Gebremedhin <hilarygebremedhn28@gmail.com>
2025-12-06 01:26:38 -0800

    Your commit message

--- server/config/passport.js
+++ server/config/passport.js
@@ -67,6 +67,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
           email: profile.emails[0].value,
           googleId: profile.id,
           avatar: profile.photos[0]?.value,
+          role: "seeker", // Default role for Google sign-in users
           isVerified: true, // Social accounts are pre-verified
         });
 
@@ -138,6 +139,7 @@ if (process.env.FACEBOOK_APP_ID && process.env.FACEBOOK_APP_SECRET) {
           email: email,
           facebookId: profile.id,
           avatar: profile.photos[0]?.value,
+          role: "seeker", // Default role for Facebook sign-in users
           isVerified: true, // Social accounts are pre-verified
         });
 
